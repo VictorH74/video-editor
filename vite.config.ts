2404f9301403a70fd5f757da6325c7b246d038ae
VictorH74 <victorh.almeida7@gmail.com>
2024-04-07 15:47:11 -0300

    fix: SharedArrayBuffer 2

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,19 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'configure-response-headers',
+      configureServer: (server) => {
+        server.middlewares.use((_req, res, next) => {
+          res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
+          res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+          next();
+        });
+      },
+    },
+  ],
   optimizeDeps: {
     exclude: ["@ffmpeg/ffmpeg", "@ffmpeg/util"],
   },
@@ -20,20 +32,3 @@ export default defineConfig({
     },
   },
 })
-
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   optimizeDeps: {
-//     exclude: ["@ffmpeg/ffmpeg", "@ffmpeg/util"],
-//   },
-//   server: {
-//     headers: {
-//       "Cross-Origin-Opener-Policy": "same-origin",
-//       "Cross-Origin-Embedder-Policy": "require-corp",
-//     },
-//   },
-// });
